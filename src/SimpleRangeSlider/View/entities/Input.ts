d0951ef46ebddcccbc286523bac56d66082d34e9
Khasanov Petr <pietroismad@gmail.com>
2020-12-11 15:12:47 +0300

    started work on Input

--- src/SimpleRangeSlider/View/entities/Input.ts
+++ src/SimpleRangeSlider/View/entities/Input.ts
@@ -1,6 +1,20 @@
 class Input {
-  constructor() {
-    console.log('input initiated');
+  type: tInputType;
+
+  $element: JQuery;
+
+  value?: number;
+
+  index?: number;
+
+  constructor(type: tInputType, $element: JQuery, value?: number, index?: number) {
+    this.type = type;
+    this.$element = $element;
+    this.value = value || undefined;
+    this.index = index || undefined;
+    if (this.type === 'tooltip' && this.value) {
+      this.$element.val(this.value);
+    }
   }
 }
 
