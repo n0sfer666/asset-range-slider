6c636358f1e6b680c3619d9f20989e421fd602d8
Khasanov Petr <pietroismad@gmail.com>
2021-04-20 19:49:35 +0300

    finished inputs tests

---
+++ src/tests/View/entities/inputs/textValue.test.ts
@@ -0,0 +1,45 @@
+import InputTextValue from '../../../../SimpleRangeSlider/View/entities/inputs/InputTextValue';
+
+function makeRandomNumber(min: number, max: number): number {
+  const tmpMin: number = Math.ceil(min);
+  const tmpMax: number = Math.floor(max);
+  return Math.round(Math.random() * (tmpMax - tmpMin + 1)) + tmpMin;
+}
+
+describe('InputTextValue.ts', () => {
+  const $testElement = $(document.createElement('input')).prop('type', 'text');
+  const testValue = makeRandomNumber(-1e4, 1e4);
+  const testIndex = Math.round(Math.random());
+  let testInputTextData: tInputTextData = {
+    value: -1e8,
+    index: -1e8,
+  };
+  const testCallback: iInputTextCallback = (inputTextData) => {
+    testInputTextData = inputTextData;
+  };
+  const testInstance = new InputTextValue($testElement, testValue, testIndex);
+
+  test('setNewValue(value)', () => {
+    const expectValue = makeRandomNumber(1e5, 1e6);
+    testInstance.setNewValue(expectValue);
+    const elementValue = Number(testInstance.$element.val());
+    expect(expectValue).toBe(elementValue);
+    expect(expectValue).toBe(testInstance.value);
+  });
+
+  test('subscribeOn(callback)', () => {
+    testInstance.subscribeOn(testCallback);
+    expect(testCallback).toEqual(testInstance.callbackList[0]);
+  });
+
+  test('handleInputTextFocusout()', () => {
+    const value = makeRandomNumber(1e6, 1e8);
+    testInstance.setNewValue(value);
+    testInstance.handleInputTextFocusout();
+    const expectData: tInputTextData = {
+      value,
+      index: testInstance.index,
+    };
+    expect(expectData).toEqual(testInputTextData);
+  });
+});
