e7f9599980fdc0c140ebf4db81d1955832e0aca1
Khasanov Petr <pietroismad@gmail.com>
2021-04-25 19:01:47 +0300

    refactored test connect + finished test pointer

--- src/tests/View/entities/connect.test.ts
+++ src/tests/View/entities/connect.test.ts
@@ -1,68 +1,66 @@
-// import Connect from '../../SimpleRangeSlider/View/entities/Connect';
+import Connect from '../../../SimpleRangeSlider/View/entities/Connect';
 
-// const normalizingCoefficient: number = 1e2;
-// function makeRandomNumber(startPosition?: number): number {
-//   let randomNumber = Math.round(Math.random() * normalizingCoefficient) / normalizingCoefficient;
-//   if (!startPosition || randomNumber > startPosition) {
-//     return randomNumber;
-//   }
-//   while (randomNumber < startPosition) {
-//     randomNumber = Math.round(Math.random() * normalizingCoefficient) / normalizingCoefficient;
-//   }
-//   return randomNumber;
-// }
+const normalizingCoefficient: number = 1e2;
 
-// describe('Connect.ts', () => {
-//   const startPosition: number = makeRandomNumber();
-//   const endPosition: number = makeRandomNumber(startPosition);
-//   const isSinglePointer: boolean = true;
-//   const isNotSinglePointer: boolean = false;
-//   const connects: Connect[] = [
-//     new Connect(0, endPosition, 'horizontal', isSinglePointer),
-//     new Connect(startPosition, endPosition, 'horizontal', isSinglePointer),
-//     new Connect(0, endPosition, 'vertical', isSinglePointer),
-//     new Connect(startPosition, endPosition, 'vertical', isSinglePointer),
-//     new Connect(0, endPosition, 'horizontal', isNotSinglePointer),
-//     new Connect(startPosition, endPosition, 'horizontal', isNotSinglePointer),
-//     new Connect(0, endPosition, 'vertical', isNotSinglePointer),
-//     new Connect(startPosition, endPosition, 'vertical', isNotSinglePointer),
-//   ];
+function makeRandomNumber(startPosition?: number): number {
+  let randomNumber = Math.round(Math.random() * normalizingCoefficient) / normalizingCoefficient;
+  if (!startPosition || randomNumber > startPosition) {
+    return randomNumber;
+  }
+  while (randomNumber < startPosition) {
+    randomNumber = Math.round(Math.random() * normalizingCoefficient) / normalizingCoefficient;
+  }
+  return randomNumber;
+}
 
-//   test('getElement()', () => {
-//     connects.forEach((connect) => {
-//       const $element: JQuery = jQuery(document.createElement('div'));
-//       $element.addClass('simple-range-slider__connect');
-//       $element.addClass(`simple-range-slider__connect_${connect.orientation}`);
-//       expect(connect.getElement()).toEqual($element);
-//     });
-//   });
+describe('Connect.ts', () => {
+  const startPosition: number = makeRandomNumber();
+  const endPosition: number = makeRandomNumber(startPosition);
+  const isSinglePointer: boolean = true;
+  const isNotSinglePointer: boolean = false;
+  const connects: Connect[] = [
+    new Connect(0, endPosition, 'horizontal', isSinglePointer),
+    new Connect(startPosition, endPosition, 'horizontal', isSinglePointer),
+    new Connect(0, endPosition, 'vertical', isSinglePointer),
+    new Connect(startPosition, endPosition, 'vertical', isSinglePointer),
+    new Connect(0, endPosition, 'horizontal', isNotSinglePointer),
+    new Connect(startPosition, endPosition, 'horizontal', isNotSinglePointer),
+    new Connect(0, endPosition, 'vertical', isNotSinglePointer),
+    new Connect(startPosition, endPosition, 'vertical', isNotSinglePointer),
+  ];
 
-//   test('setPosition(startPosition, endPosition)', () => {
-//     connects.forEach((connect) => {
-//       const testStartPosition: number = connect.startPosition;
-//       const testEndPosition: number = connect.endPosition;
-//       const start: number = Math.round(testStartPosition * normalizingCoefficient);
-//       const end: number = Math.round(testEndPosition * normalizingCoefficient);
-//       const CssValue: tCssValues[] = [{
-//         attribute: connect.orientation === 'horizontal' ? 'width' : 'height',
-//         value: `${end - start}%`,
-//       }];
-//       if (!connect.isSinglePointer) {
-//         CssValue.push({
-//           attribute: connect.orientation === 'horizontal' ? 'left' : 'top',
-//           value: `${start}%`,
-//         });
-//       }
-//       connect.setPosition(connect.startPosition, connect.endPosition);
-//       const expectCssValue: string | null = connect.$element[0].getAttribute('style');
-//       const resultCssValue: string = connect.isSinglePointer
-//         ? `${CssValue[0].attribute}: ${CssValue[0].value};`
-//         : `${CssValue[0].attribute}: ${CssValue[0].value}; ${CssValue[1].attribute}: ${CssValue[1].value};`;
-//       expect(expectCssValue).toEqual(resultCssValue);
-//       expect(connect.position).toEqual([testStartPosition, testEndPosition]);
-//     });
-//   });
-// });
-test('test', () => {
-  expect(true).toBe(true);
+  test('getElement()', () => {
+    connects.forEach((connect) => {
+      const $element: JQuery = jQuery(document.createElement('div'));
+      $element.addClass('simple-range-slider__connect');
+      $element.addClass(`simple-range-slider__connect_${connect.orientation}`);
+      expect(connect.getElement()).toEqual($element);
+    });
+  });
+
+  test('setPosition(startPosition, endPosition)', () => {
+    connects.forEach((connect) => {
+      const testStartPosition: number = connect.startPosition;
+      const testEndPosition: number = connect.endPosition;
+      const start: number = Math.round(testStartPosition * normalizingCoefficient);
+      const end: number = Math.round(testEndPosition * normalizingCoefficient);
+      const CssValue: tCssValues[] = [{
+        attribute: connect.orientation === 'horizontal' ? 'width' : 'height',
+        value: `${end - start}%`,
+      }];
+      if (!connect.isSinglePointer) {
+        CssValue.push({
+          attribute: connect.orientation === 'horizontal' ? 'left' : 'top',
+          value: `${start}%`,
+        });
+      }
+      connect.setPosition(connect.startPosition, connect.endPosition);
+      const expectCssValue: string | null = connect.$element[0].getAttribute('style');
+      const resultCssValue: string = connect.isSinglePointer
+        ? `${CssValue[0].attribute}: ${CssValue[0].value};`
+        : `${CssValue[0].attribute}: ${CssValue[0].value}; ${CssValue[1].attribute}: ${CssValue[1].value};`;
+      expect(expectCssValue).toEqual(resultCssValue);
+      expect(connect.position).toEqual([testStartPosition, testEndPosition]);
+    });
+  });
 });
--- src/tests/View/entities/pointer.test.ts
+++ src/tests/View/entities/pointer.test.ts
@@ -1,86 +1,137 @@
-// import Pointer from '../../SimpleRangeSlider/View/entities/Pointer';
+import Pointer from '../../../SimpleRangeSlider/View/entities/Pointer';
+import makeRandomNumber from '../../makeRandomNumber';
 
-// const className = 'simple-range-slider__pointer';
-// const normalizingCoefficient: number = 1e4;
-// const $document = $(document);
-// let pData: tPointerData;
+describe('Pointer.ts', () => {
+  const className = 'simple-range-slider__pointer';
+  const normalizingCoefficient: number = 1e4;
+  const $document = $(document);
+  let testPointerData: tPointerData = {
+    index: -1,
+    position: -1,
+  };
+  const orientations: tOrientation[] = ['horizontal', 'vertical', 'horizontal', 'vertical'];
+  const indexes: number[] = [0, 0, 1, 1];
+  const pointers: Pointer[] = orientations.map((orientation, index) => {
+    const position: number = Math.round(Math.random() * normalizingCoefficient)
+      / normalizingCoefficient;
+    const $testContainer = $(document.createElement('div'));
+    $document.find('body').append($testContainer);
+    return new Pointer($testContainer, orientation, position, indexes[index]);
+  });
 
-// describe('Pointer.ts', () => {
-//   const orientations: tOrientation[] = ['horizontal', 'vertical', 'horizontal', 'vertical'];
-//   const indexes: number[] = [0, 0, 1, 1];
-//   const $testContainers: JQuery[] = [
-//     $(document.createElement('div')),
-//     $(document.createElement('div')),
-//     $(document.createElement('div')),
-//     $(document.createElement('div')),
-//   ];
-//   const pointers: Pointer[] = orientations.map((orientation, index) => {
-//     const position: number = Math.round(Math.random() * normalizingCoefficient)
-//       / normalizingCoefficient;
-//     return new Pointer($testContainers[index], orientation, position, indexes[index]);
-//   });
+  test('getElement()', () => {
+    const $testElement: JQuery[] = orientations.map((orientation) => {
+      const $element: JQuery = $(document.createElement('div'));
+      $element.addClass(`${className}`);
+      $element.addClass(`${className}_${orientation}`);
+      return $element;
+    });
+    pointers.forEach(
+      (pointer, index) => expect(pointer.getElement()).toEqual($testElement[index]),
+    );
+  });
 
-//   test('getElement()', () => {
-//     const $testElement: JQuery[] = orientations.map((orientation) => {
-//       const $element: JQuery = $(document.createElement('div'));
-//       $element.addClass(`${className}`);
-//       $element.addClass(`${className}_${orientation}`);
-//       return $element;
-//     });
-//     pointers.forEach(
-//       (pointer, index) => expect(pointer.getElement()).toEqual($testElement[index]),
-//     );
-//   });
+  test('switchActive(isActive)', () => {
+    pointers.forEach((pointer) => {
+      pointer.switchActive(true);
+      expect(pointer.$element.hasClass(`${className}_active`)).toBe(true);
+      pointer.switchActive(false);
+      expect(pointer.$element.hasClass(`${className}_active`)).toBe(false);
+    });
+  });
 
-//   test('switchActive(isActive)', () => {
-//     pointers.forEach((pointer) => {
-//       pointer.switchActive(true);
-//       expect(pointer.$element.hasClass(`${className}_active`)).toBe(true);
-//       pointer.switchActive(false);
-//       expect(pointer.$element.hasClass(`${className}_active`)).toBe(false);
-//     });
-//   });
+  test('setPosition(position)', () => {
+    pointers.forEach((pointer) => {
+      const position: number = Math.round(Math.random() * normalizingCoefficient)
+        / normalizingCoefficient;
+      const liter: string = pointer.orientation === 'horizontal' ? 'X' : 'Y';
+      const expectCss: string = `translate${liter}(${position * normalizingCoefficient}%)`;
+      pointer.setPosition(position);
+      expect(pointer.$element.css('transform')).toEqual(expectCss);
+      expect(pointer.position).toBe(position);
+    });
+  });
 
-//   test('setPosition(position)', () => {
-//     pointers.forEach((pointer) => {
-//       const position: number = Math.round(Math.random() * normalizingCoefficient)
-//         / normalizingCoefficient;
-//       const liter: string = pointer.orientation === 'horizontal' ? 'X' : 'Y';
-//       const expectCss: string = `translate${liter}(${position * normalizingCoefficient}%)`;
-//       pointer.setPosition(position);
-//       expect(pointer.$element.css('transform')).toEqual(expectCss);
-//       expect(pointer.position).toBe(position);
-//     });
-//   });
+  describe('event methods', () => {
+    pointers.forEach((pointer) => {
+      const { $element } = pointer;
+      const testCallback: iPointerCallback = (pointerData: tPointerData) => {
+        testPointerData = pointerData;
+      };
 
-//   describe('event methods', () => {
-//     pointers.forEach((pointer) => {
-//       const { $element } = pointer;
-//       const testCallback: iPointerCallback = (pointerData: tPointerData) => {
-//         pData = pointerData;
-//       };
-//       const randomValue: number = Math.round(Math.random() * 1e2);
-//       const testEvent: JQuery.MouseEventBase = jQuery.Event('click');
-//       testEvent.clientX = randomValue;
-//       testEvent.clientY = randomValue;
-//       const elementOffset = $element.offset() || $element[0].getBoundingClientRect();
-//       const expectShift: number = pointer.orientation === 'horizontal'
-//         ? testEvent.clientX - elementOffset.left
-//         : testEvent.clientY - elementOffset.top;
+      pointer.subscribeOn(testCallback);
+      const testCallbackList: iPointerCallback[] = [testCallback];
 
-//       pointer.subscribeOn(testCallback);
-//       const testCallbackList: iPointerCallback[] = [testCallback];
+      test('subscribeOn(callback)', () => {
+        expect(testCallbackList).toEqual(pointer.callbackList);
+      });
 
-//       test('subscribeOn(callback)', () => {
-//         expect(testCallbackList).toEqual(pointer.callbackList);
-//       });
+      test('getShift(event)', () => {
+        const testEvent: JQuery.MouseEventBase = jQuery.Event('click');
+        testEvent.clientX = makeRandomNumber(-1e4, 1e4);
+        testEvent.clientY = makeRandomNumber(-1e4, 1e4);
+        const elementOffset = $element.offset() || $element[0].getBoundingClientRect();
+        const expectShift: number = pointer.orientation === 'horizontal'
+          ? testEvent.clientX - elementOffset.left
+          : testEvent.clientY - elementOffset.top;
+        expect(pointer.getShift(testEvent)).toEqual(expectShift);
+      });
 
-//       test('getShift(event)', () => {
-//         expect(pointer.getShift(testEvent)).toEqual(expectShift);
-//       });
-//     });
-//   });
-// });
-test('test', () => {
-  expect(true).toBe(true);
+      test('All handlers are called', () => {
+        const handler = jest.fn();
+        pointer.$element.on('mousedown', handler);
+        pointer.$element.on('mousemove', handler);
+        pointer.$element.on('mouseup', handler);
+        pointer.$element.mousedown();
+        pointer.$element.mousemove();
+        pointer.$element.mouseup();
+        expect(handler).toBeCalledTimes(3);
+      });
+
+      test('handlePointerMouseDown(event)', () => {
+        const { $container } = pointer;
+        const testEvent: JQuery.MouseEventBase = jQuery.Event('mousedown');
+        testEvent.clientX = makeRandomNumber(-1e4, 1e4);
+        testEvent.clientY = makeRandomNumber(-1e4, 1e4);
+        const containerOffset = $container.offset() || $container[0].getBoundingClientRect();
+        const boundingClientRect = pointer.orientation === 'horizontal'
+          ? containerOffset.left
+          : containerOffset.top;
+        const outerWidth = $container.outerWidth() || $container[0].offsetWidth;
+        const outerHeight = $container.outerHeight() || $container[0].offsetHeight;
+        const containerOffsetSize = pointer.orientation === 'horizontal'
+          ? outerWidth
+          : outerHeight;
+        pointer.handlePointerMouseDown(testEvent);
+        expect(boundingClientRect).toEqual(pointer.boundingClientRect);
+        expect(containerOffsetSize).toEqual(pointer.containerOffsetSize);
+      });
+
+      test('handlePointerMove(event)', () => {
+        const testEvent: JQuery.MouseEventBase = jQuery.Event('mousemove');
+        testEvent.clientX = Math.round(Math.random() * 1e2);
+        testEvent.clientY = Math.round(Math.random() * 1e2);
+        const cursorPosition = pointer.orientation === 'horizontal'
+          ? testEvent.clientX
+          : testEvent.clientY;
+        const newPosition = cursorPosition - pointer.shift - pointer.boundingClientRect;
+        const newPositionInPercent = newPosition / pointer.containerOffsetSize;
+        let position: number;
+        if (newPositionInPercent > 1) {
+          position = 1;
+        } else if (newPositionInPercent < 0) {
+          position = 0;
+        } else {
+          position = Math.round(newPositionInPercent * normalizingCoefficient)
+            / normalizingCoefficient;
+        }
+        const expectData: tPointerData = {
+          index: pointer.index,
+          position,
+        };
+        pointer.handlePointerMove(testEvent);
+        expect(expectData).toEqual(testPointerData);
+      });
+    });
+  });
 });
