1716c094d067e78b7ba87c7c88772ee3735d9046
Khasanov Petr <pietroismad@gmail.com>
2021-04-25 15:42:53 +0300

    refactored test checkboxTooltip

--- src/tests/View/entities/inputs/checkboxTooltip.test.ts
+++ src/tests/View/entities/inputs/checkboxTooltip.test.ts
@@ -1,29 +1,21 @@
-// import InputCheckboxTooltip from '../../../../SimpleRangeSlider/View/entities/inputs/InputCheckboxTooltip';
-// import Tooltip from '../../../../SimpleRangeSlider/View/entities/Tooltip';
+import InputCheckboxTooltip from '../../../../SimpleRangeSlider/View/entities/inputs/InputCheckboxTooltip';
+import Tooltip from '../../../../SimpleRangeSlider/View/entities/Tooltip';
+import makeRandomNumber from '../../../makeRandomNumber';
 
-// describe('InputCheckboxTooltip.ts', () => {
-//   const $testElement: JQuery = $(document.createElement('input')).prop('type', 'checkbox');
-//   const testTooltips: Tooltip[] = [
-//     new Tooltip(makeRandomNumber(-1e4, -1), 'horizontal'),
-//     new Tooltip(makeRandomNumber(0, 1e4), 'horizontal'),
-//   ];
-//   const testInstance = new InputCheckboxTooltip($testElement, testTooltips);
+describe('InputCheckboxTooltip.ts', () => {
+  const $testElement: JQuery = $(document.createElement('input')).prop('type', 'checkbox');
+  const testTooltips: Tooltip[] = [
+    new Tooltip(makeRandomNumber(-1e4, -1), 'horizontal'),
+    new Tooltip(makeRandomNumber(0, 1e4), 'horizontal'),
+  ];
+  const testInstance = new InputCheckboxTooltip($testElement, testTooltips);
 
-//   test('handleTooltipCheckboxChange()', () => {
-//     expect(testInstance.$element.is(':checked')).toBe(true);
-//     testInstance.handleTooltipCheckboxChange();
-//     testTooltips.forEach((tooltipInstance) => {
-//       expect(tooltipInstance.$element.css('display')).toBe('block');
-//     });
-
-//     testInstance.$element.prop('checked', false);
-//     expect(testInstance.$element.is(':checked')).toBe(false);
-//     testInstance.handleTooltipCheckboxChange();
-//     testTooltips.forEach((tooltipInstance) => {
-//       expect(tooltipInstance.$element.css('display')).toBe('none');
-//     });
-//   });
-// });
-test('test', () => {
-  expect(true).toBe(true);
+  test('handleTooltipCheckboxChange()', () => {
+    const { tooltips } = testInstance;
+    tooltips.forEach((tooltip) => {
+      testInstance.$element.click().change();
+      const isChecked = testInstance.$element.is(':checked');
+      expect(tooltip.$element.css('display')).toBe(isChecked ? 'block' : 'none');
+    });
+  });
 });
