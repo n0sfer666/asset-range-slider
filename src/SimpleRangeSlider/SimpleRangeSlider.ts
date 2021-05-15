45cde30326d7409f929ce65ef5cbc21fbb7ee8fc
Khasanov Petr <pietroismad@gmail.com>
2021-05-15 09:00:59 +0300

    refactored slider with jQ best practices

--- src/SimpleRangeSlider/SimpleRangeSlider.ts
+++ src/SimpleRangeSlider/SimpleRangeSlider.ts
@@ -81,3 +81,14 @@ class SimpleRangeSlider {
 }
 
 export default SimpleRangeSlider;
+
+// eslint-disable-next-line func-names
+(function ($: JQueryStatic) {
+  $.fn.extend({
+    simpleRangeSlider(config: iConfigUser) {
+      // eslint-disable-next-line no-unused-vars
+      const slider = new SimpleRangeSlider(<JQuery> this, <iConfigUser> config);
+      return this;
+    },
+  });
+}(jQuery));
