7cbb4ee569f8de167a19c83d23a3b30892f6219c
Khasanov Petr <pietroismad@gmail.com>
2021-05-17 16:38:45 +0300

    added compontents cleared index.ts, renamed page to pages, removed demoPanel.scss

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import './style/main.scss';
-import './style/demoPanel.scss';
 import SimpleRangeSlider from './SimpleRangeSlider/SimpleRangeSlider';
 
 // eslint-disable-next-line func-names
@@ -14,7 +13,4 @@ import SimpleRangeSlider from './SimpleRangeSlider/SimpleRangeSlider';
 }(jQuery));
 
 jQuery(document).ready(() => {
-  $('.js-slider').simpleRangeSlider({ step: 10 });
-  // eslint-disable-next-line no-unused-vars
-  const slider = new SimpleRangeSlider($('.js-slider-2'), { start: [10, 20], step: 10 });
 });
