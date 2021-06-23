c7c8cc262133a9bd6afde92870ed6fac763c709d
Khasanov Petr <pietroismad@gmail.com>
2021-06-23 11:32:36 +0300

    removed demo-panel init from index.ts #3

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
-import DemoPanel from './components/demo-panel/demo-panel';
-
 function importAll(r: any) {
   r.keys().forEach(r);
 }
 importAll(require.context('./style', true, /\.scss/));
 importAll(require.context('./components', true, /\.scss/));
-jQuery(document).ready(() => {
-  const demoPanel: DemoPanel[] = [];
-  const $demoPanels = $('.js-demo-panel');
-  $.each($demoPanels, (_, element) => {
-    demoPanel.push(new DemoPanel($(element)));
-  });
-});
