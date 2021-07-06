13f99e6fe817ec49a31b6cce52e79b2c6c30216d
Khasanov Petr <pietroismad@gmail.com>
2021-07-06 15:21:57 +0300

    added to container dataset completeConfig without input #1

--- src/SimpleRangeSlider/SimpleRangeSliderJQ.ts
+++ src/SimpleRangeSlider/SimpleRangeSliderJQ.ts
@@ -5,7 +5,16 @@ import SimpleRangeSlider from './SimpleRangeSlider';
   $.fn.extend({
     simpleRangeSlider(config: ConfigUserList) {
       // eslint-disable-next-line no-new
-      new SimpleRangeSlider(<JQuery> this, <ConfigUserList> config);
+      const slider = new SimpleRangeSlider(<JQuery> this, <ConfigUserList> config);
+      const { $container, completeConfig } = slider;
+      const configKeys: string[] = Object.keys(completeConfig);
+      const indexOfInput = configKeys.indexOf('input');
+      if (indexOfInput !== -1) {
+        configKeys.splice(configKeys.indexOf('input'));
+      }
+      configKeys.forEach((key) => {
+        $container.data(key, completeConfig[key]);
+      });
       return this;
     },
   });
