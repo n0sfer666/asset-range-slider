d04fce433f8f91aa50d978fbf8275da653a80eba
Khasanov Petr <pietroismad@gmail.com>
2021-07-09 11:37:25 +0300

    removed unnesesery eslint exception

--- src/SimpleRangeSlider/SimpleRangeSliderJQ.ts
+++ src/SimpleRangeSlider/SimpleRangeSliderJQ.ts
@@ -4,7 +4,6 @@ import SimpleRangeSlider from './SimpleRangeSlider';
 (function ($: JQueryStatic) {
   $.fn.extend({
     simpleRangeSlider(config: ConfigUserList) {
-      // eslint-disable-next-line no-new
       const slider = new SimpleRangeSlider(<JQuery> this, <ConfigUserList> config);
       const { $container, completeConfig } = slider;
       const configKeys: string[] = Object.keys(completeConfig);
