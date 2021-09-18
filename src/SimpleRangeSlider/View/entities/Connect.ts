3fb348e7d176c9efceb355b91b48ac81b1b56f29
Khasanov Petr <pietroismad@gmail.com>
2021-09-18 11:26:32 +0300

    refactored Connect #17

--- src/SimpleRangeSlider/View/entities/Connect.ts
+++ src/SimpleRangeSlider/View/entities/Connect.ts
@@ -1,7 +1,7 @@
 class Connect {
   readonly className: string = 'simple-range-slider__connect';
 
-  valuesPosition: number;
+  startPosition: number;
 
   endPosition: number;
 
@@ -11,23 +11,20 @@ class Connect {
 
   $element: JQuery;
 
-  position: [number, number];
-
   readonly normalizingCoefficient: number = 1e2;
 
   constructor(
-    valuesPosition: number,
+    startPosition: number,
     endPosition: number,
     orientation: ConfigOrientation,
     isSinglePointer: boolean,
   ) {
-    this.valuesPosition = valuesPosition;
+    this.startPosition = startPosition;
     this.endPosition = endPosition;
     this.orientation = orientation;
     this.isSinglePointer = isSinglePointer;
     this.initElement();
-    this.position = [this.valuesPosition, this.endPosition];
-    this.setPosition(this.valuesPosition, this.endPosition);
+    this.setPosition(this.startPosition, this.endPosition, isSinglePointer);
   }
 
   initElement() {
@@ -36,18 +33,19 @@ class Connect {
     this.$element.addClass(`${this.className}_${this.orientation}`);
   }
 
-  setPosition(valuesPosition: number, endPosition: number, isSinglePointer?: boolean) {
+  setPosition(startPosition: number, endPosition: number, isSinglePointer?: boolean) {
     ['width', 'height', 'left', 'top'].forEach((attr) => {
       this.$element.css(attr, '');
     });
     this.isSinglePointer = isSinglePointer !== undefined ? isSinglePointer : this.isSinglePointer;
-    const values: number = Math.round(valuesPosition * this.normalizingCoefficient);
+    const values: number = Math.round(startPosition * this.normalizingCoefficient);
     const end: number = Math.round(endPosition * this.normalizingCoefficient);
     this.$element.css(this.orientation === 'horizontal' ? 'width' : 'height', `${end - values}%`);
     if (!this.isSinglePointer) {
       this.$element.css(this.orientation === 'horizontal' ? 'left' : 'top', `${values}%`);
     }
-    this.position = [valuesPosition, endPosition];
+    this.startPosition = startPosition;
+    this.endPosition = endPosition;
   }
 
   setOrientation(orientation: ConfigOrientation) {
@@ -55,7 +53,7 @@ class Connect {
       this.$element.removeClass(`${this.className}_${this.orientation}`);
       this.orientation = orientation;
       this.$element.addClass(`${this.className}_${this.orientation}`);
-      this.setPosition(this.position[0], this.position[1]);
+      this.setPosition(this.startPosition, this.endPosition);
     }
   }
 }
