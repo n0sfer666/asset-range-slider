e328f5f8648bea07f54e41273c420ec253d0dff0
Khasanov Petr <pietroismad@gmail.com>
2021-09-18 11:17:19 +0300

    linted #2

--- src/Typings/SimpleRangeSlider.d.ts
+++ src/Typings/SimpleRangeSlider.d.ts
@@ -54,6 +54,7 @@ interface InputTextCallback {
 interface ModelCallback {
   (modelData: ModelData): void
 }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface JQuery {
   simpleRangeSlider(config: UserConfigList): JQuery
 }
