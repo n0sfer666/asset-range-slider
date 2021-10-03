446ca2297b3a999b36605b7638c714364549d8e9
Khasanov Petr <pietroismad@gmail.com>
2021-10-03 17:05:37 +0300

    added test getConfig to Presenter #20

--- src/tests/Controller/presenter.test.ts
+++ src/tests/Controller/presenter.test.ts
@@ -4,11 +4,33 @@ describe('Presenter', () => {
   const $container = $('<div></div>').appendTo(document.body);
   const instance: Presenter = new Presenter($container, {});
 
-  test('update slider', () => {
+  test('updateSlider', () => {
     const config = JSON.parse(JSON.stringify(instance.getConfig()));
     instance.updateSlider({
       range: [-1000, 1000],
     });
     expect(config).not.toEqual(instance.getConfig());
+  });
+
+  test('getConfig()', () => {
+    const testConfig: CompleteConfigList = {
+      orientation: 'vertical',
+      values: [-50, 50],
+      range: [-100, 100],
+      step: 1,
+      withConnect: true,
+      withScale: true,
+      withTooltip: true,
+    };
+    instance.updateSlider({
+      orientation: 'vertical',
+      values: [-50, 50],
+      range: [-100, 100],
+      step: 1,
+      withConnect: true,
+      withScale: true,
+      withTooltip: true,
+    });
+    expect(testConfig).toEqual(instance.getConfig());
   });
 });
