8d6b7108b8daa1fabd1cdf578c2d8f0101eaa62e
Khasanov Petr <pietroismad@gmail.com>
2021-10-03 16:41:53 +0300

    finished test Presenter #20

--- src/tests/Controller/presenter.test.ts
+++ src/tests/Controller/presenter.test.ts
@@ -1,36 +1,14 @@
+import Presenter from '../../SimpleRangeSlider/Presenter/Presenter';
+
 describe('Presenter', () => {
-  test.todo('please pass');
+  const $container = $('<div></div>').appendTo(document.body);
+  const instance: Presenter = new Presenter($container, {});
+
+  test('update slider', () => {
+    const config = JSON.parse(JSON.stringify(instance.getConfig()));
+    instance.updateSlider({
+      range: [-1000, 1000],
+    });
+    expect(config).not.toEqual(instance.getConfig());
+  });
 });
-// import Presenter from '../../SimpleRangeSlider/Presenter/Presenter';
-// import Model from '../../SimpleRangeSlider/Model/Model';
-// import View from '../../SimpleRangeSlider/View/View';
-
-// describe('Presenter.ts', () => {
-//   const values: PointerValue = [10];
-//   const range: ConfigRange = [0, 100];
-//   const step = 1;
-//   const modelConfig: ModelConfigList = { values, range, step };
-//   const model: Model = new Model(modelConfig);
-
-//   const $container = $(document.createElement('div'));
-//   $(document.body).append($container);
-//   const viewConfig: ViewConfigList = {
-//     connect: true,
-//     scale: true,
-//     tooltip: true,
-//     orientation: 'horizontal',
-//     values,
-//     range,
-//     step,
-//   };
-//   const positions: number[] = values.map(
-//     (value) => model.getPositionFromValue(value),
-//   );
-//   const view: View = new View($container, viewConfig, positions);
-
-//   const presenter: Presenter = new Presenter(view, model);
-
-//   test('presenter instanceof Presenter', () => {
-//     expect(presenter instanceof Presenter).toBe(true);
-//   });
-// });
