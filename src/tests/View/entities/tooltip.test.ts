59abca25d3ed7aa8fab9d2cb2ec58830af208993
Khasanov Petr <pietroismad@gmail.com>
2021-10-05 13:06:34 +0300

    finished test Tooltip #20

--- src/tests/View/entities/tooltip.test.ts
+++ src/tests/View/entities/tooltip.test.ts
@@ -1,29 +1,41 @@
-describe('Tooltip', () => {
-  test.todo('please pass');
+import Tooltip from '../../../SimpleRangeSlider/View/entities/Tooltip';
+import makeRandomNumber from '../../makeRandomNumber';
+
+describe('Tooltip.ts', () => {
+  const tooltips: Tooltip[] = [
+    new Tooltip(makeRandomNumber(-1e3, 1e3), 'horizontal'),
+    new Tooltip(makeRandomNumber(-1e3, 1e3), 'vertical'),
+  ];
+  const classes = {
+    root: 'simple-range-slider__tooltip',
+  };
+
+  test('initElement()', () => {
+    tooltips.forEach((tooltip) => {
+      const $element: JQuery = jQuery('<div></div>', {
+        class: `${classes.root} ${classes.root}_${tooltip.orientation}`,
+      });
+      tooltip.initElement();
+      expect(tooltip.$element).toEqual($element);
+    });
+  });
+
+  test('setValue(value)', () => {
+    tooltips.forEach((tooltip) => {
+      const testValue: number = makeRandomNumber(-1e4, 1e4);
+      tooltip.setValue(testValue);
+      expect(tooltip.value).toEqual(testValue);
+      expect(tooltip.$element.text()).toEqual(String(testValue));
+    });
+  });
+
+  test('setOrientation(orientation)', () => {
+    tooltips.forEach((tooltip) => {
+      const orientations: ConfigOrientation[] = ['horizontal', 'vertical'];
+      orientations.forEach((orientation) => {
+        tooltip.setOrientation(orientation);
+        expect(tooltip.$element.hasClass(`${classes.root}_${orientation}`)).toBe(true);
+      });
+    });
+  });
 });
-// import Tooltip from '../../../SimpleRangeSlider/View/entities/Tooltip';
-// import makeRandomNumber from '../../makeRandomNumber';
-
-// describe('Tooltip.ts', () => {
-//   const tooltips: Tooltip[] = [
-//     new Tooltip(makeRandomNumber(-1e3, 1e3), 'horizontal'),
-//     new Tooltip(makeRandomNumber(-1e3, 1e3), 'vertical'),
-//   ];
-
-//   test('getElement()', () => {
-//     tooltips.forEach((tooltip) => {
-//       const $element: JQuery = jQuery(document.createElement('div'));
-//       $element.addClass('simple-range-slider__tooltip');
-//       $element.addClass(`simple-range-slider__tooltip_${tooltip.orientation}`);
-//       expect(tooltip.getElement()).toEqual($element);
-//     });
-//   });
-
-//   test('setValue(value)', () => {
-//     tooltips.forEach((tooltip) => {
-//       const testValue: number = makeRandomNumber(-1e4, 1e4);
-//       tooltip.setValue(testValue);
-//       expect(tooltip.value).toEqual(testValue);
-//       expect(tooltip.$element.text()).toEqual(String(testValue));
-//     });
-//   });
