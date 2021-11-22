b79236f278aec7c2ba0c436798c56a64c78ffaf9
Khasanov Petr <pietroismad@gmail.com>
2021-11-22 12:11:03 +0300

    refactored presenter test #20

--- src/tests/Controller/presenter.test.ts
+++ src/tests/Controller/presenter.test.ts
@@ -1,15 +1,66 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import Presenter from '../../SimpleRangeSlider/Presenter/Presenter';
 
 describe('Presenter', () => {
-  const $container = $('<div></div>').appendTo(document.body);
-  const instance: Presenter = new Presenter($container, {});
+  let $container = $('<div></div>').appendTo(document.body);
+  let instance: Presenter = new Presenter($container, {});
+  beforeEach(() => {
+    $container = $('<div></div>').appendTo(document.body);
+    instance = new Presenter($container, {});
+  });
+  afterEach(() => {
+    $container.remove();
+  });
 
-  test('updateSlider', () => {
+  describe('view subscribe on model end vice versa', () => {
+    let spies = {
+      view: jest.spyOn(instance['view'], 'subscribeOn'),
+      model: jest.spyOn(instance['model'], 'subscribeOn'),
+    };
+    const callbacks = {
+      forView: (viewData: ViewData) => { instance['model'].updateByView(viewData); },
+      forModel: (modelData: ModelData) => { instance['view'].updateByModel(modelData); },
+    };
+
+    beforeEach(() => {
+      spies = {
+        view: jest.spyOn(instance['view'], 'subscribeOn'),
+        model: jest.spyOn(instance['model'], 'subscribeOn'),
+      };
+    });
+    afterEach(() => {
+      $.each(spies, (_, spy) => {
+        spy.mockReset().mockRestore();
+      });
+    });
+
+    test('callback for model was added', () => {
+      setTimeout(() => {
+        expect(spies.model).toHaveBeenCalledWith(callbacks.forModel);
+      }, 5);
+    });
+    test('callback for view was added', () => {
+      setTimeout(() => {
+        expect(spies.view).toHaveBeenCalledWith(callbacks.forView);
+      }, 5);
+    });
+  });
+
+  describe('updateSlider', () => {
     const config = JSON.parse(JSON.stringify(instance.getConfig()));
     instance.updateSlider({
       range: [-1000, 1000],
     });
-    expect(config).not.toEqual(instance.getConfig());
+    test('config was change', () => {
+      setTimeout(() => {
+        expect(config).not.toEqual(instance.getConfig());
+      }, 5);
+    });
+    test('config.range is [-1000, 1000]', () => {
+      setTimeout(() => {
+        expect(instance.getConfig().range).toEqual([-1000, 1000]);
+      }, 5);
+    });
   });
 
   test('getConfig()', () => {
