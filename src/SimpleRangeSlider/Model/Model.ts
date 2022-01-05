90a3f2b1e7f7f0d71a4bb71f437745284377b809
Petr Khasanov <n0sfer666@yandex.ru>
2022-01-05 17:59:33 +0300

    refactored setValueAndPosition method #23

--- src/SimpleRangeSlider/Model/Model.ts
+++ src/SimpleRangeSlider/Model/Model.ts
@@ -122,20 +122,28 @@ class Model {
   }
 
   setValueAndPosition(newValue: number, index: number) {
-    const leftStep = this.config.values[index] - (this.config.step / 2);
-    const rightStep = this.config.values[index] + (this.config.step / 2);
-    const isOutOfStep = newValue >= rightStep || newValue <= leftStep;
-    let isNotOutOfRange = newValue >= this.config.range[0] && newValue <= this.config.range[1];
-    if (typeof (this.config.values[1]) === 'number') {
-      isNotOutOfRange = index === 0
-        ? newValue >= this.config.range[0] && newValue < this.config.values[1]
-        : newValue > this.config.values[0] && newValue >= this.config.range[1];
-    }
-    const resultValue = Math.round(newValue / this.config.step) * this.config.step;
+    const { range, values, step } = this.config;
+    const leftBoundary = values[index] - (step / 2);
+    const rightBoundary = values[index] + (step / 2);
+    const isOutOfBoundary = newValue >= rightBoundary || newValue <= leftBoundary;
+    const resultValue = Math.round(newValue / step) * step;
 
-    if (isOutOfStep && isNotOutOfRange) {
-      this.config.values[index] = resultValue;
-      this.positions[index] = this.getPositionFromValue(resultValue);
+    if (isOutOfBoundary) {
+      if (!this.isSinglePointer && typeof values[1] === 'number') {
+        const isNotOutOfRange = index === 0
+          ? resultValue >= range[0] && resultValue <= values[1]
+          : resultValue >= values[0] && resultValue <= range[1];
+        if (isNotOutOfRange) {
+          values[index] = resultValue;
+          this.positions[index] = this.getPositionFromValue(resultValue);
+        }
+      } else {
+        const isNotOutOfRange = resultValue >= range[0] && resultValue <= range[1];
+        if (isNotOutOfRange) {
+          this.config.values[index] = resultValue;
+          this.positions[index] = this.getPositionFromValue(resultValue);
+        }
+      }
     }
   }
 
